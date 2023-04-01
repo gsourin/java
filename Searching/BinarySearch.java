6fff22f44bee4c956e34097e3313924f3eb6c9c2
gsourin <mr.souringhosh@gmail.com>
2023-04-01 12:59:40 +0530

    update

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
 public class BinarySearch {
-    
     public static int binarysearch(int e, int arr[], int f, int s) {
         int l = s, h = e;
-        while (l<=h) {
+        while (l <= h) {
             int mid = (l + h) / 2;
             if (arr[mid] == f)
                 return mid;
             else if (arr[mid] < f)
-                l= mid+1;
+                l = mid + 1;
             else
-                h= mid-1;
+                h = mid - 1;
         }
-        return -1;
+        return -1; 
     }
-
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of Array: ");
@@ -29,14 +28,11 @@ public class BinarySearch {
         int f = sc.nextInt();
         int s = 0;
         int x = binarysearch(l - 1, arr, f, s);
-
-        
         if (x == -1) {
-        System.out.println("Element not found: ");
+            System.out.println("Element not found: ");
         } else {
-        System.out.println("Element found at potion: " + x);
+            System.out.println("Element found at potion: " + x);
         }
-
         sc.close();
     }
 }
