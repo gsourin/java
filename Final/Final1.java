fd29c91ffab6aded88c79a049f88b272d738bf20
gsourin <mr.souringhosh@gmail.com>
2023-04-30 23:36:59 +0530

    Update

---
+++ Final/Final1.java
@@ -0,0 +1,9 @@
+//package Final;
+class Final1 {
+    public static void main(String[] args) {
+        new Honda().run();
+    }    
+}
+class Honda extends Final1{
+    final void run(){System.out.println("Running:");}
+}
