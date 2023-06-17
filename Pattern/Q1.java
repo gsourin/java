2ee6601c89ab51e4f3a32c137c891c6df24a15f7
gsourin <mr.souringhosh@gmail.com>
2023-06-18 02:53:24 +0530

    Update

--- Pattern/Q1.java
+++ Pattern/Q1.java
@@ -13,9 +13,11 @@
 
 public class Q1 {
     public static void main(String[] args) {
-        for(int i=0;i<4;i++)
+        int m=4;
+        int n=5;
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<5;j++)
+            for(int j=0;j<n;j++)
             {
                 System.out.print("*");
             }
