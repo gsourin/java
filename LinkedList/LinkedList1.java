b02219af5121b32ba4712fac350679dabdb080fa
gsourin <mr.souringhosh@gmail.com>
2023-06-24 21:38:35 +0530

    Update

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -59,6 +59,94 @@ public class LinkedList1 {
         tail=newNode;
     }
 
+    //Add Middle
+
+    public void addMiddle(int idx,int data)
+    {
+        //If One Node Is Presnt
+        if(idx==0)
+        {
+            addFirst(data);
+            return;
+        }
+
+        //Create A New Node
+        Node newNode= new Node(data);
+        size++;
+
+        Node temp=head;
+        int i=0;
+        while (i<idx-1)
+        {
+            temp=temp.next;
+            i++;
+        }
+
+        //i=idx-1 ; temp->prev
+        newNode.next=temp.next;
+        temp.next=newNode;
+
+    }
+
+    //Remove First
+
+    public int removeFirst()
+    {
+        if(size==0)
+        {
+            System.out.println("Linked List Is Empty: ");
+            return Integer.MIN_VALUE;
+        }
+
+        else if(size==1)
+        {
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        else
+        {
+            int val=head.data;
+            head=head.next;
+            size--;
+            return val;
+        }
+        
+    }
+
+    //Remove At Last
+    public int removeLast()
+    {
+      if(size==0)
+        {
+            System.out.println("Linked List Is Empty: ");
+            return Integer.MIN_VALUE;
+        }
+
+        else if(size==1)
+        {
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        else
+        {
+        //prev; i=size-2
+        Node prev=head;
+        for(int i=0;i<size-2;i++)
+        {
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+        }
+    }
+
     //Print LinkedList
     public void print()
     {
@@ -81,7 +169,14 @@ public class LinkedList1 {
         ll.addFirst(10);
         ll.addLast(4);
         ll.addLast(8);
+        ll.addMiddle(1,6);
         ll.print();
         System.out.println("LinkedList size: "+ll.size);
+        ll.removeFirst();
+        ll.print();
+        System.out.println("After Removing Size: "+ll.size);
+        ll.removeLast();
+        ll.print();
+        System.out.println("After Removing Size: "+ll.size);
     }
 }
